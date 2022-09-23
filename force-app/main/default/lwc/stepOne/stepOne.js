9f3db2b4134e09268f0cb54f6dfe107b0e021fb8
Vladislav Raenok <admin@gmail.com>
2022-09-23 16:49:00 +0300

    add Work Type form

--- force-app/main/default/lwc/stepOne/stepOne.js
+++ force-app/main/default/lwc/stepOne/stepOne.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
-
-export default class StepOne extends LightningElement {}
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import NAME_FIELD from '@salesforce/schema/WorkType.Name';
+import DESCRIPTION_FIELD from '@salesforce/schema/WorkType.Description';
+import DURATION_FIELD from '@salesforce/schema/WorkType.EstimatedDuration';
+import TYPE_FIELD from '@salesforce/schema/WorkType.DurationType';
+import AUTO_FIELD from '@salesforce/schema/WorkType.ShouldAutoCreateSvcAppt';
+export default class StepOne extends LightningElement {
+    @api objectApiName;
+    nameField = NAME_FIELD;
+    descriptionField = DESCRIPTION_FIELD;
+    durationField = DURATION_FIELD;
+    typeField = TYPE_FIELD;
+    autoField = AUTO_FIELD;
+    // fields = [NAME_FIELD, DESCRIPTION_FIELD, DURATION_FIELD];
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: "Work Type created",
+            variant: "success"
+        });
+        this.dispatchEvent(evt);
+    }
+}
