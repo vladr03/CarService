0d111af99a19a0050e95e9b5e0f434b821a593b5
Vladislav Raenok <admin@gmail.com>
2022-09-24 15:12:36 +0300

    add Skill Requirement Form

--- force-app/main/default/lwc/stepOne/stepOne.js
+++ force-app/main/default/lwc/stepOne/stepOne.js
@@ -1,12 +1,11 @@
 import { LightningElement, api } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/WorkType.Name';
 import DESCRIPTION_FIELD from '@salesforce/schema/WorkType.Description';
 import DURATION_FIELD from '@salesforce/schema/WorkType.EstimatedDuration';
 import TYPE_FIELD from '@salesforce/schema/WorkType.DurationType';
 import AUTO_FIELD from '@salesforce/schema/WorkType.ShouldAutoCreateSvcAppt';
-export default class StepOne extends NavigationMixin(LightningElement) {
+export default class StepOne extends LightningElement {
     @api objectApiName;
     nameField = NAME_FIELD;
     descriptionField = DESCRIPTION_FIELD;
@@ -15,16 +14,15 @@ export default class StepOne extends NavigationMixin(LightningElement) {
     autoField = AUTO_FIELD;
     // fields = [NAME_FIELD, DESCRIPTION_FIELD, DURATION_FIELD];
     isFormCompleted = false;
+    workTypeId;
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: "Work Type created",
             variant: "success"
         });
         this.dispatchEvent(evt);
-    }
-
-    theSecondStepHandler() {
+        this.workTypeId = event.detail.id;
         this.isFormCompleted = true;
-        console.log("test");
+        console.log(this.workTypeId);
     }
 }
--- force-app/main/default/lwc/stepTwo/stepTwo.js
+++ force-app/main/default/lwc/stepTwo/stepTwo.js
@@ -1,4 +1,13 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import RELATED_FIELD from '@salesforce/schema/SkillRequirement.RelatedRecordId';
+import SKILL_FIELD from '@salesforce/schema/SkillRequirement.SkillId';
+import LEVEL_FIELD from '@salesforce/schema/SkillRequirement.SkillLevel';
 
 export default class StepTwo extends LightningElement {
+    @api objectApiName;
+    @api workTypeId;
+    relatedField = RELATED_FIELD;
+    skillField = SKILL_FIELD;
+    levelField = LEVEL_FIELD;
 }
