c53b6b6ecbc9d0c3c21c562b7a3a6646b5643095
Vladislav Raenok <admin@gmail.com>
2022-09-24 14:15:34 +0300

    created the second lwc

--- force-app/main/default/lwc/stepOne/stepOne.js
+++ force-app/main/default/lwc/stepOne/stepOne.js
@@ -1,11 +1,12 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/WorkType.Name';
 import DESCRIPTION_FIELD from '@salesforce/schema/WorkType.Description';
 import DURATION_FIELD from '@salesforce/schema/WorkType.EstimatedDuration';
 import TYPE_FIELD from '@salesforce/schema/WorkType.DurationType';
 import AUTO_FIELD from '@salesforce/schema/WorkType.ShouldAutoCreateSvcAppt';
-export default class StepOne extends LightningElement {
+export default class StepOne extends NavigationMixin(LightningElement) {
     @api objectApiName;
     nameField = NAME_FIELD;
     descriptionField = DESCRIPTION_FIELD;
@@ -13,11 +14,17 @@ export default class StepOne extends LightningElement {
     typeField = TYPE_FIELD;
     autoField = AUTO_FIELD;
     // fields = [NAME_FIELD, DESCRIPTION_FIELD, DURATION_FIELD];
+    isFormCompleted = false;
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: "Work Type created",
             variant: "success"
         });
         this.dispatchEvent(evt);
+    }
+
+    theSecondStepHandler() {
+        this.isFormCompleted = true;
+        console.log("test");
     }
 }
