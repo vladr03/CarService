f90da6e8b9bf64a0771a50013282949e2df58200
Vladislav Raenok <admin@gmail.com>
2022-09-25 22:39:05 +0300

    created Work Order form

---
+++ force-app/main/default/lwc/workOrderOne/workOrderOne.js
@@ -0,0 +1,24 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import STATUS_FIELD from '@salesforce/schema/WorkOrder.Status';
+import PRIORITY_FIELD from '@salesforce/schema/WorkOrder.Priority';
+import TYPE_FIELD from '@salesforce/schema/WorkOrder.WorkTypeId';
+import DESCRIPTION_FIELD from '@salesforce/schema/WorkOrder.Description';
+import SUBJECT_FIELD from '@salesforce/schema/WorkOrder.Subject';
+
+export default class WorkOrderOne extends LightningElement {
+    statusField = STATUS_FIELD;
+    priorityField = PRIORITY_FIELD;
+    typeField = TYPE_FIELD;
+    descriptionField = DESCRIPTION_FIELD;
+    subjectField = SUBJECT_FIELD;
+    isFormCompleted = false;
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: "Work Order created",
+            variant: "success"
+        });
+        this.isFormCompleted = true;
+        this.dispatchEvent(evt);
+    }
+}
